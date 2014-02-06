45371344d47a0fb9fcbeb12f0ae83ee69a3b117c
Martin DENIZET <martin.denizet@supinfo.com>
2014-02-06 17:01:20 +0800

    Fix automatic conversion of the HTML entities. Thanks Jeremy Merritt.

--- lib/redmine_custom_css/hooks.rb
+++ lib/redmine_custom_css/hooks.rb
@@ -2,7 +2,7 @@ module RedmineCustomCss
   class RedmineLoginAuditHookListener < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context)
       css = Setting.plugin_redmine_custom_css['css']
-      content_tag(:style, css, :type => 'text/css') unless css.nil? or css.empty?
+      "<style type=\"text/css\">#{css}</style>" unless css.nil? or css.empty?
     end
   end
 end
