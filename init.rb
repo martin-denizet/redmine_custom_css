656d034fbbf67e955a74704d8d378d787d81deb6
Martin DENIZET <martin.denizet@supinfo.com>
2014-02-06 17:06:02 +0800

    Version bump.

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_custom_css do
   name 'Redmine Custom CSS plugin'
   author 'Martin DENIZET'
   description 'Allows to customize Redmine\'s CSS'
-  version '0.1.1'
+  version '0.1.2'
   url 'https://github.com/martin-denizet/redmine_custom_css'
   author_url 'http://martin-denizet.com'
 
